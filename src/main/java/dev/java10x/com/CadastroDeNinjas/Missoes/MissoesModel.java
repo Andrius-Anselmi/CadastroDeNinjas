e214ca58887b2fcd56c07f177e427877fa68e660
andrius anselmi <andriusanselmi12@gmail.com>
2025-01-07 22:14:02 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/com/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/com/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,16 +1,21 @@
 package dev.java10x.com.CadastroDeNinjas.Missoes;
-
 import dev.java10x.com.CadastroDeNinjas.Ninja.NinjaModel;
-
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
 
 @Entity // As anotations elas vão ler ou até fechar as chaves ou até o ;
 
 @Table(name = "tb_missoes") // A anotation table transforma isso em uma tabela
 // tudo que esta dentro da classe MissoesModel é uma entidade
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class MissoesModel {
 
     @Id
@@ -23,32 +28,6 @@ public class MissoesModel {
     // Toda vez que usarmos um relacionamento de tabelas usaremos o mappedBy para mapear as tabelas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificulade) {
-        this.nome = nome;
-        this.dificulade = dificulade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificulade() {
-        return dificulade;
-    }
-
-    public void setDificulade(String dificulade) {
-        this.dificulade = dificulade;
-    }
-
-
 
 }
 
--- src/main/java/dev/java10x/com/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/com/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -1,14 +1,18 @@
 package dev.java10x.com.CadastroDeNinjas.Ninja;
-
 import dev.java10x.com.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity // TRANSFORMA A CLASSE EM ENTIDADE no BD(Banco de Dados)
 //JPA = Java Persistence API
 @Table(name = "tb_cadastro")
-
+@NoArgsConstructor // criamos um construtor sem argumentos
+@AllArgsConstructor// criamos um construtor com todos os argumentos
+@Data // cria os getter e os setters
 public class NinjaModel{
 
 
@@ -26,41 +30,8 @@ public class NinjaModel{
     @ManyToOne
     //JoinColumn serve pra gente unir as tabelas de cadastro ninja e cadastro de missoes
     @JoinColumn(name = "missoes_id") // foreing key ou chave estrangeira
-
     // chave estrangeira sempre vai aparecer quando tivermos duas ou mais tabelas se relacionando
+
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
